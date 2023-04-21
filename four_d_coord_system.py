dc7c5ee471747d2ec3d56693c4ef81ae13d4fa0d
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-04-21 19:24:32 +0200

    Create four_d_coord_system.py

---
+++ four_d_coord_system.py
@@ -0,0 +1,9 @@
+import sys
+import json
+
+# Global variables
+current_pos = [0, 0, 0, 0]
+greetings = {
+    (0, 0, 0, 0): "Welcome to the origin!",
+    (2, 2, 2, 2): "Congratulations, you found the secret location!"
+}
