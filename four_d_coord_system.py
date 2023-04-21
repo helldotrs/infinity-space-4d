6bb94db996ce0bc04eac08cb9de7d37bade762c2
Rebekah <79146603+hellmak@users.noreply.github.com>
2023-04-21 19:26:24 +0200

    Update four_d_coord_system.py

--- four_d_coord_system.py
+++ four_d_coord_system.py
@@ -7,3 +7,22 @@ greetings = {
     (0, 0, 0, 0): "Welcome to the origin!",
     (2, 2, 2, 2): "Congratulations, you found the secret location!"
 }
+
+def main():
+    # Check for command-line arguments
+    if len(sys.argv) > 1:
+        command = sys.argv[1]
+        if command == "move":
+            move(sys.argv[2:])
+        elif command == "show":
+            show()
+        elif command == "save":
+            save(sys.argv[2])
+        elif command == "load":
+            load(sys.argv[2])
+        else:
+            print("Invalid command")
+            print_usage()
+    else:
+        print("No command specified")
+        print_usage()
